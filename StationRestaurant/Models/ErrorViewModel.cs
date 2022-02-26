d39d5776a387e3890e240eb9d4bf9611d745712f
Cavid Hacıyev <hcavid386@gmail.com>
2022-02-25 23:58:44 -0800

    Add project files.

---
+++ StationRestaurant/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StationRestaurant.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
