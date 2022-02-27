528510952e104723276184c0694b31219c882abb
Cavid Hacıyev <hcavid386@gmail.com>
2022-02-27 05:05:30 -0800

    IProductRepository :rocket:

--- Data/Repositories/Interfaces/IProductRepository.cs
+++ Data/Repositories/Interfaces/IProductRepository.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using System.Threading.Tasks;
 using Core.Entities;
 
 namespace Data.Repositories.Interfaces
 {
     public interface IProductRepository
     {
-        List<Product> GetAll(Expression<Func<Product,bool>> expression=null); 
+        List<Product> GetAll(Expression<Func<Product, bool>> expression = null, params string[] Includes);
+
+        List<Product> GetAllPaginated(int page, int size, Expression<Func<Product, bool>> expression = null,
+            params string[] Includes);
+
+        Product Get(Expression<Func<Product, bool>> expression = null, params string[] Includes);
+        int GetTotalCount(Expression<Func<Product, bool>> expression = null);
+        bool IsProductExist(Expression<Func<Product, bool>> expression = null);
+        Task CreateAsync(Product product);
+        Task UpdateAsync(Product product);
+        void Remove(Product product);
+        Task SaveAsync();
+
     }
 }
