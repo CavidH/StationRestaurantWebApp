36f3dff8ed7331b351d4f75053efe5a5b8351009
Cavid Hacıyev <hcavid386@gmail.com>
2022-02-28 05:58:34 -0800

    Repository pattern :rocket:

--- Data/Repositories/Implementations/ProductRepository.cs
+++ Data/Repositories/Implementations/ProductRepository.cs
@@ -11,86 +11,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories.Implementations
 {
-    public class ProductRepository : IProductRepository
+    public class ProductRepository : Repository<Product>, IProductRepository
     {
         private readonly AppDbContext _context;
-        //private AppDbContext _context { get; }
 
-        public ProductRepository(AppDbContext context)
+        public ProductRepository(AppDbContext context):base(context)
         {
             _context = context;
         }
 
-        public async Task<Product> Get(Expression<Func<Product, bool>> expression = null, params string[] Includes)
-        {
-            //IQueryable<Product> query = _context.Products.AsQueryable();
-            var query = CheckQuery(Includes);
-            return expression is null
-                ? await query.FirstOrDefaultAsync()
-                : await query.Where(expression).FirstOrDefaultAsync();
-        }
-
-        public async Task<List<Product>> GetAllAsync(Expression<Func<Product, bool>> expression = null, params string[] Includes)
-        {
-            var query = CheckQuery(Includes);
-            return expression is null
-                ? await query.ToListAsync()
-                : await query.Where(expression).ToListAsync();
-        }
-
-        public async Task<List<Product>> GetAllPaginatedAsync(int page, int size, Expression<Func<Product, bool>> expression = null, params string[] Includes)
-        {
-            var query = CheckQuery(Includes);
-            return expression is null
-                ? await query.Skip((page - 1) * size).Take(size).ToListAsync()
-                : await query.Where(expression).Skip((page - 1) * size).Take(size).ToListAsync();
-        }
-
-        public async Task<int> GetTotalCountAsync(Expression<Func<Product, bool>> expression = null)
-        {
-            return expression is null
-                ? await _context.Products.CountAsync()
-                : await _context.Products.Where(expression).CountAsync();
-        }
-
-        public async Task<bool> IsProductExistAsync(Expression<Func<Product, bool>> expression)
-        {
-                 return   await _context.Products.AnyAsync(expression);
-        }
-
-        public Task CreateAsync(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task UpdateAsync(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Remove(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task SaveAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        private IQueryable<Product> CheckQuery(params string[] Includes)
-        {
-            var query = _context.Products.AsQueryable();
-
-            if (Includes != null)
-            {
-                foreach (var include in Includes)
-                {
-                    query.Include(include);
-                }
-            }
-
-            return query;
-        }
     }
+
 }
--- Data/Repositories/Interfaces/IProductRepository.cs
+++ Data/Repositories/Interfaces/IProductRepository.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
-using Core.Entities;
+﻿using Core.Entities;
+using Data.DAL;
 
 namespace Data.Repositories.Interfaces
 {
-    public interface IProductRepository
+    public interface IProductRepository :IRepository<Product>
     {
-        Task<Product> Get(Expression<Func<Product, bool>> expression = null, params string[] Includes);
-        Task<List<Product>> GetAllAsync(Expression<Func<Product, bool>> expression = null, params string[] Includes);
-        Task<List<Product>> GetAllPaginatedAsync(int page, int size, Expression<Func<Product, bool>> expression = null, params string[] Includes);
-        Task<int> GetTotalCountAsync(Expression<Func<Product, bool>> expression = null);
-        Task<bool> IsProductExistAsync(Expression<Func<Product, bool>> expression);
-        Task CreateAsync(Product product);
-        Task UpdateAsync(Product product);
-        void Remove(Product product);
-        Task SaveAsync();
+       
+
 
     }
 }
