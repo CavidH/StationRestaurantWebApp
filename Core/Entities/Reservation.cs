a9f03b09d066b66c70cbcc71f8b656336fda09aa
Cavid Hacıyev <hcavid386@gmail.com>
2022-03-02 04:43:30 -0800

    rezerv and Table model done :rocket:

---
+++ Core/Entities/Reservation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities
+{
+    public class Reservation
+    {
+        public int  Id { get; set; }
+        public string Name  { get; set; }
+        public string LastName  { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime ReservDate { get; set; }
+        public bool IsActive { get; set; }
+
+
+
+
+        public int TableID { get; set; }
+        public Table Table { get; set; }
+
+    }
+}
---
+++ Core/Entities/Table.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Core.Entities
+{
+    public class Table
+    {
+        public int Id { get; set; }
+        public int TableNumber { get; set; }
+        public int MaxPersonCount { get; set; }   
+        public bool IsReserved { get; set; }  
+        public bool IsDeleted { get; set; }
+
+
+
+        public int ReservationId { get; set; }
+        public Reservation Reservation { get; set; }
+    }
+}
