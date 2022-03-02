9cbd5ba539655463573b9ac8348eba86467d3ca9
Cavid Hacıyev <hcavid386@gmail.com>
2022-03-02 04:43:08 -0800

    Db Type configurations done:rocket:

--- Data/Configurations/AboutConfiguration.cs
+++ Data/Configurations/AboutConfiguration.cs
@@ -4,11 +4,14 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Data.Configurations
 {
-    public class AboutConfiguration:IEntityTypeConfiguration<About>
+    public class AboutConfiguration : IEntityTypeConfiguration<About>
     {
         public void Configure(EntityTypeBuilder<About> builder)
         {
-            throw new System.NotImplementedException();
+            builder.Property(p => p.Title).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.Head).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.Content).IsRequired().HasColumnType("TEXT()");
+            builder.Property(p => p.UpdatedAt).IsRequired().HasDefaultValueSql("GETUTCDATE()");
         }
     }
 }
---
+++ Data/Configurations/ReservationConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Data.Configurations
+{
+   public class ReservationConfiguration:IEntityTypeConfiguration<Reservation>
+    {
+        public void Configure(EntityTypeBuilder<Reservation> builder)
+        {
+            builder.Property(p => p.Name).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.LastName).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.Email).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.IsActive).IsRequired().HasDefaultValue(true);
+            builder.Property(p => p.PhoneNumber).IsRequired().HasMaxLength(255);
+            builder.Property(p => p.ReservDate).IsRequired();
+        }
+    }
+}
