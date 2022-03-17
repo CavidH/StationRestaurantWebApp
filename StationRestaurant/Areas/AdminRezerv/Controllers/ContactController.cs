b6ce5a49e6c51b33fb2306137b0248f0a7f3cdb7
Cavid Hacıyev <hcavid386@gmail.com>
2022-03-18 00:43:37 +0400

    :bomb:

--- StationRestaurant/Areas/AdminRezerv/Controllers/ContactController.cs
+++ StationRestaurant/Areas/AdminRezerv/Controllers/ContactController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Business.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StationRestaurant.Areas.AdminRezerv.Controllers
-{
+{   [Area("AdminRezerv")]
+    [Authorize]
     public class ContactController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnitOfWorkService _unitOfWorkService;
+
+        public ContactController(IUnitOfWorkService unitOfWorkService)
         {
-            return View();
+            _unitOfWorkService = unitOfWorkService;
+        }
+
+        public async Task<IActionResult> Index(int page=1)
+        {
+            var contacts = await _unitOfWorkService
+                .contactService
+                .GetAllPaginatedAsync(page);
+            return View(contacts);
         }
     }
 }
